ca0150b9dfc60578c1b722263671526604419866
kevin chiu <kevinchiu@kevins-MacBook-Pro.local>
2024-08-08 23:16:37 +0800

    home page skeleton change, add mdsvex library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,19 @@
 
 </script>
 
-<div class="bg-primary-bg h-screen text-white">
-    <div class="pt-[4%] mx-[17%] flex">
-        <h1 class="text-4xl font-bold"> WiHi Blog </h1>
+<div class="bg-primary-bg h-screen text-white flex flex-col flex-grow">
+    <div class="pt-[4%] mx-[17%]">
+
+        <div class="text-center mb-3">
+            <h1 class="pb-1"> Date </h1>
+            <h1 class="text-4xl"> Heading </h1>
+        </div>
+
+        <hr class="border-neutral-300 mb-5"/>
+
+        <div class="grid grid-cols-3 gap-4">
+            <div class="">05</div>
+            <div class="col-span-2 ..."> Text goes here </div>
+        </div>
     </div>
 </div>
