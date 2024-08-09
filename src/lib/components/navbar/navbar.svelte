e8dc8bfb9ed814d33f37cbb072bdd0254ebae714
kevin chiu <kevinchiu@kevins-MacBook-Pro.local>
2024-08-09 14:36:32 +0800

    blog template structure change

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -11,7 +11,7 @@
   
   <div class="absolute z-50 text-white w-full flex justify-center">
       <nav class="bg-transparent/40 shadow-lg drop-shadow-lg backdrop-blur w-full">
-          <div class="relative flex justify-between h-12 items-center mx-[17%]"f >
+          <div class="relative flex justify-between h-12 items-center mx-[20%]"f >
   
             <div class="flex flex-shrink-0 items-center">
               <a href="https://www.wihi.cc/" class="ml-2 font-medium text-lg"> <img alt class="h-8 w-auto lg:block" src={logo}> </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,18 @@
 </script>
 
 <div class="bg-primary-bg h-screen text-white flex flex-col flex-grow">
-    <div class="pt-[4%] mx-[17%]">
+    <div class="pt-[4%] mx-[20%]">
 
-        <div class="text-center mb-5">
-            <h1 class="pb-1"> Date </h1>
+        <div class="mb-5">
             <h1 class="text-4xl font-semibold"> Heading </h1>
+            <h1 class="pb-1 text-gray-300"> Date </h1>
         </div>
 
         <hr class="border-neutral-300 mb-7"/>
 
         <div class="grid grid-cols-3 gap-4">
-            <div class="">05</div>
             <div class="col-span-2 ..."> Text goes here </div>
+            <div class="">05</div>
         </div>
     </div>
 </div>
